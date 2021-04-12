7d480745e91433419b86c08d3a93126f3e7d599f
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2021-04-12 10:11:25 +0200

    work on sneakers view

--- src/views/Sneakers.vue
+++ src/views/Sneakers.vue
@@ -1,9 +1,54 @@
 <template>
-  <div></div>
+  <div>
+    <NavBar />
+    <SubNav />
+    <FilterSection @options="setSelectedOptions" />
+    <!--  <Shoe /> -->
+    <h1>{{ selectedOptions }}</h1>
+    <h1>{{ filterSneakers() }}</h1>
+  </div>
 </template>
 
 <script>
-export default {};
+import NavBar from '@/components/nav/NavBar.vue';
+import SubNav from '@/components/nav/SubNav.vue';
+import FilterSection from '@/components/FilterSection.vue';
+import Shoe from '@/components/Shoe.vue';
+import shoesData from '../../public/data.json';
+import { useShoeFilter } from '@/shared-logic/useShoeFilter';
+export default {
+  components: {
+    NavBar,
+    SubNav,
+    FilterSection,
+    Shoe,
+  },
+
+  setup() {
+    const { selectedOptions, setSelectedOptions } = useShoeFilter();
+
+    function filterSneakers() {
+      const shoes = shoesData.filter((shoe) => shoe.type === 'sneakers');
+      if (
+        selectedOptions.color === 'All colors' &&
+        selectedOptions.size === 'All sizes'
+      )
+        return shoes;
+      else {
+        return shoes.filter((shoe) => {
+          return (
+            (selectedOptions.color === 'All colors' ||
+              selectedOptions.color === shoe.color) &&
+            (selectedOptions.size === 'All sizes' ||
+              shoe.size.includes(selectedOptions.size))
+          );
+        });
+      }
+    }
+
+    return { selectedOptions, setSelectedOptions, filterSneakers };
+  },
+};
 </script>
 
 <style lang="scss" scoped></style>
